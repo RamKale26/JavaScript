c74e4bf6a8dc38ed72797e5d5f8ad0c3e7baf4cd
RAM <kaleramnath2003@gmail.com>
2025-04-28 16:24:57 +0530

    Sum of Number

---
+++ Java_Script__Program/SumOfNumber.js
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+/*1. Sum of Numbers
+Description: Write a program to calculate the sum of numbers from 1 to n.
+Input: n = 5
+Output: 15
+*/
+
+
+let a = 1;
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+	sum = i + sum;
+	a++;
+}
+console.log(sum);
+
+=======
+/*1. Sum of Numbers
+Description: Write a program to calculate the sum of numbers from 1 to n.
+Input: n = 5
+Output: 15
+*/
+
+
+let a = 1;
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+	sum = i + sum;
+	a++;
+}
+console.log(sum);
+
+>>>>>>> d95e09586369fa7e7dd55ec3d190d2ffc2f68747
